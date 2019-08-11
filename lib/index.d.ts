93e60e841bae3b9632a17916c3d9df102d1b590b
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-08-10 19:45:39 -0600

    debug undefined classname issue

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
-export declare const combineClasses: (...classes: (string | [boolean, string])[]) => string;
+export declare const combineClasses: (...classes: (string | [boolean, string] | undefined)[]) => string;
 export default combineClasses;
 //# sourceMappingURL=index.d.ts.map
